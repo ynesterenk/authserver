6bae0a95067a35fab570907f445cd77ab164f1e2
ynesterenk <yevgen2005@gmail.com>
2024-09-09 16:06:43 +0300

    Radical commit

--- authorization-service-server/src/main/java/com/github/vitalibo/authorization/server/core/UserPool.java
+++
@@ -1,9 +0,0 @@
-package com.github.vitalibo.authorization.server.core;
-
-public interface UserPool {
-
-    String authenticate(String username, String password) throws UserPoolException;
-
-    void changePassword(String username, String previousPassword, String proposedPassword) throws UserPoolException;
-
-}
--- authorization-service-shared/src/main/java/shared/core/validation/Rule.java
+++
@@ -1,7 +0,0 @@
-package com.github.vitalibo.authorization.shared.core.validation;
-
-import java.util.function.BiConsumer;
-
-public interface Rule<T> extends BiConsumer<T, ErrorState> {
-
-}
